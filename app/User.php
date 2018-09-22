a633cde0cc0a75479ddfdc180390f6d468fc09e3
Raul Palacios <palacios.raul02@gmail.com>
2018-09-22 12:17:14 -0500

    add authorization logic

--- app/User.php
+++ app/User.php
@@ -13,6 +13,34 @@ class User extends Authenticatable
         return $this->belongsToMany('LaraDex\Role');
     }
     
+    public function authorizeRoles($roles){
+        if($this->hasAnyRole($roles)){
+            return true;
+        }
+        abort(401, 'This action is unauthorized');
+    }
+
+    public function hasAnyRole($roles){
+        if(is_array($roles)){
+            foreach ($roles as $role) {
+                if($this->hasRole($role)){
+                    return true;
+                }    
+            }
+        } else {
+            if($this->hasRole($roles)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function hasRole($role){
+        if($this->roles()->where('name',$role)->first()){
+            return true;
+        }
+        return false;
+    }
     /**
      * The attributes that are mass assignable.
      *
